13d8bb45419ca936c25310ef54422bffbc7cd29d
Joseph Teichman <jteich3@gmail.com>
2022-03-27 17:12:54 -0400

    scripts from macos, linux

---
+++ linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Starts the Arduino IDE using the docker image.
+# see also: ...
+
+#    -v /dev/ttyACM0:/dev/ttyACM0 \
+#    -v /dev/ttyUSB0:/dev/ttyUSB0 \
+docker run \
+    -it \
+    --rm \
+    --network=host \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -v /dev:/dev \
+    -v $HOME/topics:/topics \
+    -v $HOME/topics/arduino:/home/developer/Arduino \
+    --name arduino \
+    jteich/darduino:latest  \
+    arduino
+
